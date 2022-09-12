8884dd83405eab8dc32b7c037b9ac4bbdbe21aa2
Mr Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-09-12 23:37:39 +0530

    Create InputandOutput.cpp

---
+++ CPP/InputandOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b,c,d;
+    cin>>a>>b>>c;
+    d = a+b+c;
+    cout<<d;
+    
+    return 0;
+}
