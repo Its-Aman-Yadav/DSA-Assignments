438867dce6e5ac4703ffca8b5017c0817aea30b5
Mr Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-09-12 23:57:15 +0530

    Create Pointers.cpp

---
+++ CPP/Pointers.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int absDifference = *a - *b > 0 ? *a - *b : -(*a - *b);
+    *a = sum;
+    *b = absDifference; 
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
