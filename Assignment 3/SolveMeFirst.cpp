236cfbd640fcce8fb37f47be59930d94a4a042b3
Mr Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-10-06 23:14:17 +0530

    Create SolveMeFirst.cpp

---
+++ Assignment 3/SolveMeFirst.cpp
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int sum1=0;
+  sum1=a+b;
+  return sum1;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
