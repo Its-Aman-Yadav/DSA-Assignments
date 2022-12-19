6f4bb4ad3dec337cb771832231d92710c9b15d63
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-12-19 22:24:40 +0530

    Create Binary Search.cpp

---
+++ Binary Search/Binary Search.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/binary-search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+     int low=0, high=nums.size()-1;
+      
+     while(low<=high){
+       int mid=high-low/2;
+       if(nums[mid]==target) return mid;
+       else if(nums[mid]>target) high=mid-1;
+       else low = mid+1;
+     }
+      
+     return -1;
+    
+    }
+};
