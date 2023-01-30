36db347d4f42e8951a79e038b77ce507944ab86c
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-01-30 22:59:53 +0530

    Create Merge Two Sorted Array.cpp

---
+++ Assignment 8.1 - Sorting/Merge Two Sorted Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i,j,current;
+      
+    for(int i=m;i<m+n;i++)
+       nums1[i]=nums2[i-m];
+      
+    for(j=m;j<m+n;j++){
+       int current=nums1[j];
+       int k=j-1;
+      
+    while (k>=0 && nums1[k]>current){
+    nums1[k+1]=nums1[k];
+    k--;
+     }
+      
+     nums1[k+1]=current;
+     }
+   }
+};
